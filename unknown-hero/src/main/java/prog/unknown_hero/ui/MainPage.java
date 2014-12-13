e72cfbc489b0089ba8bc9cdca8cb731bf684b60d
elvis@asus <zeroasclin@gmail.com>
2014-12-13 23:07:30 +0800

    Main page finished.

--- unknown-hero/src/main/java/prog/unknown_hero/ui/MainPage.java
+++ unknown-hero/src/main/java/prog/unknown_hero/ui/MainPage.java
@@ -1,9 +1,14 @@
 package prog.unknown_hero.ui;
 
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+
+
+
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -15,20 +20,48 @@ public class MainPage extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public MainPage() {
-		setTitle("變身英雄");
-		setSize(800, 600);
+	public MainPage(String title, int width, int height) {
+		setTitle(title);
+		setSize(width, height);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		JPanel panel = new JPanel();
+		
 		BufferedImage background;
 		try {
-			background = ImageIO.read(new File(".\\img\\2014-10-06 01.21.00.jpg"));
-			setContentPane(new ImagePanel(resize(background, 800, 600)));
+			background = ImageIO.read(new File(".\\img\\F-3.jpg"));
+			panel = new ImagePanel(background);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		setContentPane(panel);
+
+		ImageIcon buttonImage_def = new ImageIcon(".\\img\\B-NOT CLICK.jpg");
+		ImageIcon buttonImage_hov = new ImageIcon(".\\img\\B-CLICK.jpg");
+		
+		panel.setLayout(null);
+		JButton button = new JButton(buttonImage_def);
+		button.setSize(322, 300);
+		button.setBounds(239, 300, 322, 300);
+		panel.add(button);
+		
+		button.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				button.setIcon(buttonImage_hov);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				button.setIcon(buttonImage_def);
+			}
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				new WaitingPage(title, width, height);
+				dispose();
+			}
+		});
 		
 		setVisible(true);
 		
