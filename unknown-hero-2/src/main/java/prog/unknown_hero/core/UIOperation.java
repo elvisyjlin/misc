a73d39780d766f948c78990bd5e85e53d1e395dc
elvis@asus <zeroasclin@gmail.com>
2014-12-14 07:46:00 +0800

    op

---
+++ unknown-hero-2/src/main/java/prog/unknown_hero/core/UIOperation.java
@@ -0,0 +1,42 @@
+package prog.unknown_hero.core;
+
+import prog.unknown_hero.utility.BaseMessage;
+import prog.unknown_hero.utility.Sender;
+
+public class UIOperation {
+
+	public static void setPlayerHandCards() {
+		
+	}
+	
+	public static void setPlayerHero() {
+		
+	}
+	
+	public static void setPlayerHP() {
+		
+	}
+	
+	public static void setPlayerAP() {
+		
+	}
+	
+	public static void setPlayerDP() {
+		
+	}
+	
+	public static void setPlayerWeapon() {
+		
+	}
+	
+	public static void setInformation(String info) {
+		Sender.send(new BaseMessage("INFO", info));
+	}
+	
+	public static void setPlayerHandCardsNum() {
+		Sender.send();
+	}
+	
+	
+	
+}
