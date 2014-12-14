03d3af22bf72013eb1f202c01d474636303a2b8f
elvis@asus <zeroasclin@gmail.com>
2014-12-14 08:09:00 +0800

    hello, i am elvis lol

--- unknown-hero-2/src/main/java/prog/unknown_hero/core/UIOperation.java
+++ unknown-hero-2/src/main/java/prog/unknown_hero/core/UIOperation.java
@@ -4,37 +4,51 @@ import prog.unknown_hero.utility.BaseMessage;
 import prog.unknown_hero.utility.Sender;
 
 public class UIOperation {
+	
+	public static void setPlayersOrder(int[] order) {
+		Sender.send(new BaseMessage("ORDER", order[0]+","+order[1]+","+order[2]+","+order[3]));
+	}
 
-	public static void setPlayerHandCards() {
-		
+	public static void setPlayerHandCards(int[] cards) {
+		StringBuffer s = new StringBuffer("");
+		for(int c : cards) {
+			s.append(c);
+			s.append(",");
+		}
+		s.deleteCharAt(s.length()-1);
+		Sender.send(new BaseMessage("SETHAND", s.toString()));
 	}
 	
-	public static void setPlayerHero() {
-		
+	public static void setPlayerHero(int player, int card) {
+		Sender.send(new BaseMessage("SETHERO", player+","+card));
 	}
 	
-	public static void setPlayerHP() {
-		
+	public static void setPlayerHP(int player, int hp) {
+		Sender.send(new BaseMessage("SETHP", player+","+hp));
 	}
 	
-	public static void setPlayerAP() {
-		
+	public static void setPlayerAP(int player, int ap) {
+		Sender.send(new BaseMessage("SETHP", player+","+ap));
 	}
 	
-	public static void setPlayerDP() {
-		
+	public static void setPlayerDP(int player, int dp) {
+		Sender.send(new BaseMessage("SETHP", player+","+dp));
 	}
 	
-	public static void setPlayerWeapon() {
-		
+	public static void setPlayerWeapon(int player, int card) {
+		Sender.send(new BaseMessage("SETWP", player+","+card));
+	}
+	
+	public static void delPlayerWeapon(int player) {
+		Sender.send(new BaseMessage("DELWP", Integer.toString(player)));
 	}
 	
 	public static void setInformation(String info) {
-		Sender.send(new BaseMessage("INFO", info));
+		Sender.send(new BaseMessage("SETINFO", info));
 	}
 	
-	public static void setPlayerHandCardsNum() {
-		Sender.send();
+	public static void setPlayerHandCardsNum(int player, int num) {
+		Sender.send(new BaseMessage("SETHANDN", player+","+num));
 	}
 	
 	
