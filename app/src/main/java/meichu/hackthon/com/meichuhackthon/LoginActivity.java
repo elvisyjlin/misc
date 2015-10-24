7f655897d76d715f312ef8fc91245ea34bdc014d
elvis@asus <zeroasclin@gmail.com>
2015-10-25 07:11:25 +0800

    push

--- app/src/main/java/meichu/hackthon/com/meichuhackthon/LoginActivity.java
+++ app/src/main/java/meichu/hackthon/com/meichuhackthon/LoginActivity.java
@@ -1,15 +1,21 @@
 package meichu.hackthon.com.meichuhackthon;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
@@ -27,15 +33,34 @@ import org.json.JSONObject;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends Activity {
+public class LoginActivity extends AppCompatActivity {
 
     CallbackManager callbackManager;
     private AccessToken accessToken;
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,14 +73,37 @@ public class LoginActivity extends Activity {
         //宣告callback Manager
         callbackManager = CallbackManager.Factory.create();
 
-        //找到button
-        Button loginButton = (Button) findViewById(R.id.fb_login);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        loginButton.setOnClickListener(new Button.OnClickListener(){
+        // Hide the status bar
+//        View decorView = getWindow().getDecorView();
+//        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+//        ActionBar actionBar = getActionBar();
+//        actionBar.hide();
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.login_container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
 
             @Override
-            public void onClick(View v) {
-                LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Arrays.asList("public_profile", "user_friends"));
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // swipe to page 2
+                if (state == 0 && mViewPager.getCurrentItem() == 1) {
+                    LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Arrays.asList("public_profile", "user_friends"));
+                }
             }
         });
 
@@ -72,29 +120,49 @@ public class LoginActivity extends Activity {
 
                 Log.d("FB", "access token got.");
 
-                //send request and call graph api
-                GraphRequest request = GraphRequest.newMeRequest(
-                        accessToken,
-                        new GraphRequest.GraphJSONObjectCallback() {
+//                //send request and call graph api
+//                GraphRequest request = GraphRequest.newMeRequest(
+//                        accessToken,
+//                        new GraphRequest.GraphJSONObjectCallback() {
+//
+//                            //當RESPONSE回來的時候
+//                            @Override
+//                            public void onCompleted(JSONObject object, GraphResponse response) {
+//
+//                                //讀出姓名 ID FB個人頁面連結
+//                                Log.d("FB", "complete");
+//                                Log.d("FB", object.optString("name"));
+//                                Log.d("FB", object.optString("link"));
+//                                Log.d("FB", object.optString("id"));
+//
+//                            }
+//                        });
 
-                            //當RESPONSE回來的時候
+//                //包入你想要得到的資料 送出request
+//                Bundle parameters = new Bundle();
+//                parameters.putString("fields", "id,name,link");
+//                request.setParameters(parameters);
+//                request.executeAsync();
+
+                Log.i("Token", accessToken.getToken());
+
+
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("token", "123");
+//                params.put("token", accessToken.getToken());
+
+                MyHttpConnection.sendPostRequest(MyHttpConnection.DEFAULT_DB, params, "UTF-8", new MyHttpConnection.Callback() {
+                    @Override
+                    public void handle(String response) {Log.i("test", response);
+                       // Log.i("response", response);
+                        runOnUiThread(new Runnable() {
                             @Override
-                            public void onCompleted(JSONObject object, GraphResponse response) {
-
-                                //讀出姓名 ID FB個人頁面連結
-                                Log.d("FB", "complete");
-                                Log.d("FB", object.optString("name"));
-                                Log.d("FB", object.optString("link"));
-                                Log.d("FB", object.optString("id"));
-
+                            public void run() {
+                                Toast.makeText(LoginActivity.this, "recv", Toast.LENGTH_LONG);
                             }
                         });
-
-                //包入你想要得到的資料 送出request
-                Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,name,link");
-                request.setParameters(parameters);
-                request.executeAsync();
+                    }
+                });
 
                 runOnUiThread(new Runnable() {
                     @Override
@@ -159,6 +227,76 @@ public class LoginActivity extends Activity {
 
         // Logs 'app deactivate' App Event.
         AppEventsLogger.deactivateApp(this);
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+            }
+            return null;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
     }
 
 }
--- app/src/main/java/meichu/hackthon/com/meichuhackthon/MainActivity.java
+++ app/src/main/java/meichu/hackthon/com/meichuhackthon/MainActivity.java
@@ -3,6 +3,14 @@ package meichu.hackthon.com.meichuhackthon;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +20,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private static CustomViewPager mViewPager;
+    private static NavigationView navigationView;
+
+    /* The following strings are for test. They need retrieving from MySQL. */
+    private static String userName = "User";
+    private static String account = "user@facebook.com";
+    private static String[] test_strings = {"Hello.", "I am Elvis.", "Ha ha!", "I don't like you...", "LOL",
+                                        "Hmm...", "Oops!", "](-_-)[", "So sad.", "Bye!"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +71,42 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.getMenu().getItem(0).setChecked(true);
+
+        View headerLayout = navigationView.inflateHeaderView(R.layout.nav_header_main);
+        TextView textView = (TextView) headerLayout.findViewById(R.id.navTextView);
+        textView.setText(userName);
+        textView = (TextView) headerLayout.findViewById(R.id.navSubTextView);
+        textView.setText(account);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (CustomViewPager) findViewById(R.id.main_container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // swipe to page 2
+                if (state == 0 && mViewPager.getCurrentItem() == 1) {
+
+                }
+            }
+        });
     }
 
     @Override
@@ -80,14 +147,20 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_diary) {
+            getSupportFragmentManager().popBackStack(0, 0);
+            backToDiary();
+        } else if (id == R.id.nav_calendar) {
+            Fragment calendarFragment = new CalendarFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.add(R.id.main_container, calendarFragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+            mViewPager.setSwipeable(false);
+        } else if (id == R.id.nav_hashtag) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+//        } else if (id == R.id.nav_manage) {
+//
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
@@ -98,4 +171,91 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 10 total pages.
+            return 10;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "SECTION "+(position+1);
+        }
+    }
+
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.content_label);
+            textView.setText(test_strings[getArguments().getInt(ARG_SECTION_NUMBER) - 1]);
+            Log.i("onCreateView", "i=" + getArguments().getInt(ARG_SECTION_NUMBER));
+            backToDiary();
+            return rootView;
+        }
+    }
+
+    public static class CalendarFragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.calendar_main, container, false);
+            return view;
+        }
+
+    }
+
+    private static void backToDiary() {
+        if(!mViewPager.getSwipeable()) {
+            mViewPager.setSwipeable(true);
+        }
+        navigationView.getMenu().getItem(0).setChecked(true);
+    }
+
 }
